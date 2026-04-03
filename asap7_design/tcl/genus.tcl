7ea7914d87e96d4bbd9f5c13bb38053a74bf912c
Dosang2108 <minhsang210805@gmail.com>
2026-04-03 16:11:36 +0700

    Update code and remove unused files

--- asap7_design/tcl/genus.tcl
+++ asap7_design/tcl/genus.tcl
@@ -1,15 +1,15 @@
-set ASAP7_DIR "../../../Asap7/asap7"
+set ASAP7_DIR "../../Asap7/asap7"
 set LIB_PATH "${ASAP7_DIR}/asap7sc7p5t_28/LIB/NLDM"
 
 # 1. Setup Library
 set_db library [glob ${LIB_PATH}/*.lib]
 #
 # # 2. Load Design
-read_hdl ../rtl/Adder32.v
+read_hdl rtl/Adder32.v
 elaborate Adder32
 #
 # # 3. Apply Constraints
-read_sdc constraint.sdc
+read_sdc tcl/constraint.sdc
 #
 # # 4. Synthesis Flow
 syn_generic
@@ -17,12 +17,12 @@ syn_map
 syn_opt
 #
 # # 5. Reports (Theo trang 57 tài li?u)
-report_area > ../reports/synthesis_area.rpt
-report_gates > ../reports/synthesis_gate.rpt
-report_power > ../reports/synthesis_power.rpt
-report_timing > ../reports/synthesis_pretim.rpt
+report_area > reports/synthesis_area.rpt
+report_gates > reports/synthesis_gate.rpt
+report_power > reports/synthesis_power.rpt
+report_timing > reports/synthesis_pretim.rpt
 #
 # # 6. Export Netlist
-write_hdl > ../outputs/synthesis_net.v
-write_sdc > ../outputs/synthesis.sdc
+write_hdl > outputs/synthesis_net.v
+write_sdc > outputs/synthesis.sdc
 quit
